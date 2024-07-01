ba17f2db7bb189459cbb5ba3da3254cae3552eb6
Piyush1638 <21bcs1638@cuchd.in>
2024-07-01 09:53:02 +0530

    Added sort icons

--- src/components/SortButtons.tsx
+++ src/components/SortButtons.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { IoIosArrowUp } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
+
 
 const SortButtons = ({
   handleSort,
@@ -7,22 +10,34 @@ const SortButtons = ({
 }) => (
   <div className="flex items-center justify-evenly gap-2">
     <button
-      className="border-gray-800 border rounded-[5px] shadow-md px-3 py-1 text-sm"
+      className="border-gray-800 border rounded-[5px] flex items-center justify-between shadow-md px-3 py-1 text-sm"
       onClick={() => handleSort("postId")}
     >
-      Sort Post ID
+      Sort Post ID 
+      <div className="flex flex-col gap-0 ms-2">
+        <IoIosArrowUp />
+        <IoIosArrowDown />
+      </div>
     </button>
     <button
-      className="border-gray-800 border rounded-[5px] shadow-md px-3 py-1 text-sm"
+      className="border-gray-800 border rounded-[5px] flex items-center justify-between shadow-md px-3 py-1 text-sm"
       onClick={() => handleSort("name")}
     >
       Sort Name
+      <div className="flex flex-col gap-0 ms-2">
+        <IoIosArrowUp />
+        <IoIosArrowDown />
+      </div>
     </button>
     <button
-      className="border-gray-800 border rounded-[5px] shadow-md px-3 py-1 text-sm"
+      className="border-gray-800 border rounded-[5px] flex items-center justify-between shadow-md px-3 py-1 text-sm"
       onClick={() => handleSort("email")}
     >
       Sort Email
+      <div className="flex flex-col gap-0 ms-2">
+        <IoIosArrowUp />
+        <IoIosArrowDown />
+      </div>
     </button>
   </div>
 );
